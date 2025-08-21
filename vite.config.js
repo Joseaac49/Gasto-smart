f1ecbdb316974935762d1bce9c31417d189bc8df
Joseaac49 <tobi1234.ja@gmail.com>
2025-08-20 22:11:01 -0300

    nuevas conexiones

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+export default defineConfig({
+  plugins: [react()],
+  base: '/Gasto-smart/',   // ¡con G mayúscula!
+})
 
-export default defineConfig({ plugins:[react()], base:'/Gasto-smart/' })
 
